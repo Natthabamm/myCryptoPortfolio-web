40fe6c5c7695c71e54d4f7e5052f7e9adec783f1
natthabam <natthamonbam@gmail.com>
2022-02-03 17:29:04 +0700

    register confirm and fix login, register form

--- src/pages/RegisterConfirm.js
+++ src/pages/RegisterConfirm.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/registerConfirm/RegisterConfirm.css";
+import complete from "../pics/registerConfirm/complete.png";
+import rocket from "../pics/Rocket.png"
+import ellipse from "../pics/Ellipse.png"
 
 const RegisterConfirm = () => {
   return (
-      <>
-        <div className='confirm-register'>
-            <h1>Registration successfully!</h1>
-            <Link to="/login">Return to Log in</Link>
+    <>
+      <div className='main-register-comfirm'>
+        <div className="head-register-confirm-main">
+          <img className="rocket-register-confirm" src={rocket} atl='rocket'/>
         </div>
-      </>
+        <div className='confirm-register-div'>
+          <img src={complete} alt='complete' width={80} height={80} />
+          <h2 className='h2-confirm-register'>Registration successfully!</h2>
+          <h5 className='h5-register-confirm'>
+            Thank you for joining us. Let's enjoy managing your portfolio.
+          </h5>
+          <Link className='link-confirm-register' to='/login'>
+            Return to Log in
+          </Link>
+        </div>
+        <div className="footer-register-confirm">
+          <img className="ellipse-regidter-confirm" src={ellipse} alt="ellipse" width={350} height={300} />
+        </div>
+      </div>
+    </>
   );
 };
 
